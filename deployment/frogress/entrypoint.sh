25f771cdd720766d2de2d6361072422f109ec384
Ethan Roseman <ethteck@gmail.com>
2022-11-22 23:18:32 +0900

    more

--- deployment/frogress/entrypoint.sh
+++ deployment/frogress/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-DB_HOST=${DATABASE_HOST:-postgres}
-DB_PORT=${DATABASE_PORT:-5432}
-
-BE_HOST=${BACKEND_HOST:-frogress}
-BE_PORT=${BACKEND_PORT:-8000}
-
-until nc -z ${DB_HOST} ${DB_PORT} > /dev/null; do
-    echo "Waiting for database to become available on ${DB_HOST}:${DB_PORT}..."
+until nc -z ${DATABASE_HOST} ${DATABASE_PORT} > /dev/null; do
+    echo "Waiting for database to become available on ${DATABASE_HOST}:${DATABASE_PORT}..."
     sleep 1
 done
 
