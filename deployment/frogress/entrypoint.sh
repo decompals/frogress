db6b97461700007fe4fecd872cbfa36ddda51bc2
Ethan Roseman <ethteck@gmail.com>
2022-11-23 02:01:24 +0900

    whoopz

--- deployment/frogress/entrypoint.sh
+++ deployment/frogress/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-until nc -z ${DATABASE_HOST} ${DATABASE_PORT} > /dev/null; do
-    echo "Waiting for database to become available on ${DATABASE_HOST}:${DATABASE_PORT}..."
+until nc -z ${POSTGRES_HOST} ${POSTGRES_PORT} > /dev/null; do
+    echo "Waiting for database to become available on ${POSTGRES_HOST}:${POSTGRES_PORT}..."
     sleep 1
 done
 
